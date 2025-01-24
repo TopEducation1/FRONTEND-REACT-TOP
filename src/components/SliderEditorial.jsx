31e00f66e0bf1dff72b452a3dfcb297d170dc822
felipemadeit <felipemadeit@gmail.com>
2025-01-24 14:42:29 -0500

    RESPONSVIE EN HOME

--- src/components/SliderEditorial.jsx
+++ src/components/SliderEditorial.jsx
@@ -73,23 +73,18 @@ const SliderEditorial = () => {
         <div className="container-slider-top">
             <div className="wrapper-slide" ref={containerRef}>
                 <a href="https://info.top.education/es-mx/como-encontrar-trabajo-con-poca-experiencia" target="_blank" className="card-product">
-                    <span className="label-type">Guía</span>
                     <img src="https://images.squarespace-cdn.com/content/v1/654306c68517a21d500a928b/c2013e1d-caf8-4923-a748-cc1486259df2/Guia+Como+encontrar+trabajo+con+poca+experiencia.png?format=300w" alt="product-img"/>
                 </a>
                 <a href="https://info.top.education/ebook-construye-tu-marca-personal-con-%C3%A9xito" target="_blank" className="card-product">
-                    <span className="label-type">eBook</span>
                     <img src="https://images.squarespace-cdn.com/content/v1/654306c68517a21d500a928b/500ef00b-9648-4713-8ee4-d242416969c8/eBook+Construye+tu+marca+personal+con+exito.png?format=300w" alt="product-img"/>
                 </a>
                 <a href="https://info.top.education/ebook-crea-tu-ruta-de-aprendizaje-virtual-personalizada" target="_blank" className="card-product">
-                    <span className="label-type">eBook</span>
                     <img src="https://images.squarespace-cdn.com/content/v1/654306c68517a21d500a928b/d57b30ad-b920-450a-b076-849d8724878b/eBook+Crea+tu+ruta+de+aprendizaje+virtual+personalizada.png?format=300w" alt="product-img"/>
                 </a>
                 <a href="https://info.top.education/estrategias-para-aprender-online" target="_blank" className="card-product">
-                    <span className="label-type">eBook</span>
                     <img src="https://images.squarespace-cdn.com/content/v1/654306c68517a21d500a928b/ce4f1a0f-edaf-406f-9aec-3341a0685fb1/eBook+Estrategias+para+aprender+online.png?format=300w" alt="product-img"/>
                 </a>
                 <a href="https://info.top.education/capacitaci%C3%B3n-empresarial-del-futuro-e-learning-para-el-desarrollo-empresarial" target="_blank" className="card-product">
-                    <span className="label-type">Paper</span>
                     <img src="https://images.squarespace-cdn.com/content/654306c68517a21d500a928b/aed11f9d-1ff5-4fa1-8993-40d1e9ef0a55/Whitepaper+-+Capacitacio%CC%81n+empresarial+del+futuro+elearning+para+el+desarrollo+profesional.png?content-type=image%2Fpng" alt="product-img"/>
                 </a>
             </div>
