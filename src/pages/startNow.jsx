7649f1e5669706e46456f4ac95d02179ca571aea
felipemadeit <felipemadeit@gmail.com>
2025-02-03 11:20:41 -0500

    DISEÑO DE EMPIEZA AHORA

--- src/pages/startNow.jsx
+++ src/pages/startNow.jsx
@@ -22,10 +22,11 @@ function StartNow() {
 
             <div id="second-prices-section">
 
-                <div id="wrapper-title-second-prices-section"></div>
-                <h1>¿Por qué <em>Top Education</em> es la mejor opción?</h1>
-
-                <p><em>"Por menos de la mitad del precio que gastarias pagando edX, MasterClass y Coursera individualmente, obtienes acceso a las tres plataformas con Top Education".</em></p>
+                <div id="wrapper-title-second-prices-section">
+                    <h1><mark>¿Por qué <strong>Top Education</strong> es la mejor opción?</mark></h1>
+                    <img src="/assets/Piezas/ellipse-start-now-1.png" alt="ellipse" />
+                    <img src="/assets/Piezas/ellipse-2-startnow.png" alt="ellipse" />
+                </div>
 
                 <div id="wrapper-images-prices">
                     <img src="/assets/Piezas/price-1.png" />
@@ -35,7 +36,12 @@ function StartNow() {
 
 
             <div id="third-section-startnow">
-                <h1><em>¡Haz la prueba Top Education ahora!</em></h1>
+                <div id="wrapper-title-third-section">
+                    <h1><em>¡Haz la prueba ahora!</em></h1>
+                    <img src="/assets/Piezas/ellipse-4-startnow.png" alt="ellipse" />
+                    <img src="/assets/Piezas/ellipse-3-startnow.png" alt="ellipse" />
+                </div>
+                
 
                 <div id="wrapper-trial-images">
                     <img src="/assets/banners/Plan mensual Top Education.webp"/>
@@ -45,44 +51,25 @@ function StartNow() {
 
 
             <div id="fourth-info-section">
-                <div class="subinfo-section" id="first-subinfo-section">
-                    <div id="container-text-subinfo">
-                        <h2 style={{
-                            color: "#034694"
-                        }}>En Top Education, creemos en el poder transformador de la educación personalizada y en comunidad</h2>
 
-                        <p>En esta plataforma encontrarás una forma de aprendizaje que te permite explorar tus pasiones, conectar con expertos globales en cada campo de interés y aprender a tu ritmo sin las restricciones del sistema educativo tradicional, y alcanzar tus metas profesionales y personales.</p>
 
-                        <p>Nuestros estudiantes disfrutan de una<strong>experiencia educativa única</strong> que los ayuda a <strong>destacar en su vida personal y profesional.</strong></p>
-
-                        <p style={{
-                            color: "#034694"
-                        }}><em>Únete a nuestra comunidad y descubre como Top Education puede guiarte hacia un futuro lleno de logros y satisfacción personal</em></p>
-                    </div>
-                    <div id="container-image-subinfo">
-                        <img src="/assets/Piezas/subinfo-1.png"/>
-                    </div>
-
+                <div id="wrapper-title-fourth-info-section">
+                    <h1>¿Eres el responsable de un <span>equipo profesional?</span></h1>
                 </div>
-                <div class="subinfo-section" id="second-subinfo-section">
+
+                <div id="wrapper-p-image-info-section">
+                <div id="container-text-subinfo">
+
+                <p>Top Education para Equipos es la plataforma elegida por líderes empresariales para el desarrollo profesional efectivo de equipos.<br/><br/>Podrás certificar a tu equipo en diversas habilidades clave para potenciar su productividad, competitividad y crecimiento.<br/>También, podrás monitorizar su progreso en tiempo real, acceder a descuentos por volumen en suscripciones corporativas, establecer tu propia universidad corporativa virtual y disfrutar de contenido exclusivo diseñado para fortalecer habilidades blandas como la negociación.<br/><br/><strong><em>Nuestra plataforma ha demostrado impulsar resultados empresariales significativos en numerosas organizaciones.</em></strong></p>
+                </div>
+                <img src="/assets/Piezas/boy-image-p.png" alt="boy-image" />
+                </div>
                 
-                    <div id="container-image-subinfo">
-                        <img id="image-subinfo-toScale" src="/assets/Piezas/subinfo-2.png"/>
-                    </div>
 
-                    <div id="container-text-subinfo">
-                        <h2 style={{
-                            color: "#034694"
-                        }}>¿Eres el responsable de un equipo profesional?</h2>
-
-                        <p>Top Education para Equipos es la plataforma elegida por líderes empresariales para el desarrollo profesional efectivo de equipos.<br/><br/>Podrás certificar a tu equipo en diversas habilidades clave para potenciar su productividad, competitividad y crecimiento.<br/>También, podrás monitorizar su progreso en tiempo real, acceder a descuentos por volumen en suscripciones corporativas, establecer tu propia universidad corporativa virtual y disfrutar de contenido exclusivo diseñado para fortalecer habilidades blandas como la negociación.<br/><br/><strong><em>Nuestra plataforma ha demostrado impulsar resultados empresariales significativos en numerosas organizaciones.</em></strong></p>
-
-                        
+                
 
                     
-                    </div>
                 </div>
-            </div>
 
             <div id="fifth-team-section">
 
