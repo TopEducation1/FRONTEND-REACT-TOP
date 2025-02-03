5b49930cfdce4771a5cfccaa0773a51a14b5d20a
felipemadeit <felipemadeit@gmail.com>
2025-02-03 15:53:48 -0500

    API JS CONFIGURADO DE NUEVO

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-const API_URL = "http://127.0.0.1:8000";
-//https://backend-django-top-production.up.railway.app
+const API_URL = "https://backend-django-top-production.up.railway.app";
+//http://127.0.0.1:8000
 //console.log('API_URL:', API_URL);
 
 const BASE_URL = API_URL;
