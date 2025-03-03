75a415029d93641b7892de5aa620460552484300
felipemadeit <felipemadeit@gmail.com>
2025-03-03 17:29:49 -0500

    apple-touch-icon

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,6 @@
 const API_URL = "https://backend-django-top-production.up.railway.app";
-//http://127.0.0.1:8000
+
+//http://127.0.0.1:8000 LOCAL HOST API URL FOR TESTING
 
   const BASE_URL = API_URL;
 
