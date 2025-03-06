7f74f2e0a64fd420247f6988eb7e689764780a54
felipemadeit <felipemadeit@gmail.com>
2025-03-06 15:41:52 -0500

    CAFAM CHANGES

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_URL = "https://backend-django-top-production.up.railway.app";
 
-//http://127.0.0.1:8000 LOCAL HOST API URL FOR TESTING
+// LOCAL HOST API URL FOR TESTING
 
 const BASE_URL = API_URL;
 
